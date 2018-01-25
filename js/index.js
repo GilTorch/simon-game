882ed03df147ad4ebe4629a3e43ff753e366a61e
Gilbert <gilbert.torchon@esih.edu>
2018-01-25 11:40:49 -0500

    Passage de canvas pur a un framework: createJS

--- js/index.js
+++ js/index.js
@@ -1,57 +1,61 @@
-var canvas = document.getElementById("simon-game");
-var c = canvas.getContext("2d");
+var canvas=document.getElementById("simon-game");
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
-var width=window.innerWidth;
-var height=window.innerHeight;
-canvas.width=width;
-canvas.height=height;
-//Simon Game Design
-//OuterCircle
-var outerRadius=200;
-var x=(canvas.width-outerRadius)/2;
-var y=(canvas.height-outerRadius)/2;
-c.beginPath();
-c.arc(x,y,outerRadius,0,Math.PI*2);
-c.fillStyle="gray";
-c.fill();
-//Buttons
-//Button1
-var x=(canvas.width-outerRadius)/2;
-var y=(canvas.height-outerRadius)/2;
-c.beginPath();
-c.arc(x,y,190,0,Math.PI/2);
-c.lineTo(x,y);
-c.fillStyle="#00AA00";
-c.fill();
-//Button2
-var x=(canvas.width-outerRadius)/2;
-var y=(canvas.height-outerRadius)/2;
-c.beginPath();
-c.arc(x,y,190,Math.PI/2,Math.PI);
-c.lineTo(x,y);
-c.fillStyle="#AA00FF";
-c.fill();
-//Button3
-var x=(canvas.width-outerRadius)/2;
-var y=(canvas.height-outerRadius)/2;
-c.beginPath();
-c.arc(x,y,190,Math.PI,-Math.PI/2);
-c.lineTo(x,y);
-c.fillStyle="#AA0000";
-c.fill();
-//Button4
-var x=(canvas.width-outerRadius)/2;
-var y=(canvas.height-outerRadius)/2;
-c.beginPath();
-c.arc(x,y,190,-Math.PI/2,0);
-c.lineTo(x,y);
-c.fillStyle="#0000AA";
-c.fill();
-//Inner Circle
-var innerRadius=80;
-var xInnerCircle=(canvas.width-outerRadius)/2;
-var yInnerCircle=(canvas.height-outerRadius)/2;
-c.beginPath();
-c.arc(xInnerCircle,yInnerCircle,innerRadius,0,Math.PI*2);
-c.fillStyle="white";
-c.fill();
+function init(){
+  alert("called");
+  var stage = new createjs.Stage("simon-game");
+  var outerCircle = new createjs.Shape();
+  var simonGameContainer=new createjs.Container();
+  outerCircle.graphics.f(createjs.Graphics.getRGB(0x333333));
+  outerCircle.graphics.moveTo(0,0)
+  outerCircle.graphics.arc(0,0,200,0,Math.PI*2);
+  outerCircle.x=100;
+  outerCircle.y=100;
+  simonGameContainer.addChild(outerCircle);
+
+  var redButton=new createjs.Shape();
+  redButton.graphics.f(createjs.Graphics.getRGB(0xFF0000));
+  redButton.graphics.moveTo(0,0)
+  redButton.graphics.arc(0,0,180,0,Math.PI/2);
+  redButton.x=100;
+  redButton.y=100;
+  simonGameContainer.addChild(redButton);
+
+  var greenButton=new createjs.Shape();
+  greenButton.graphics.f(createjs.Graphics.getRGB(0x00FF00));
+  greenButton.graphics.moveTo(0,0)
+  greenButton.graphics.arc(0,0,180,Math.PI/2,Math.PI);
+  greenButton.x=100;
+  greenButton.y=100;
+  stage.addChild(greenButton);
+
+  var blueButton=new createjs.Shape();
+  greenButton.graphics.f(createjs.Graphics.getRGB(0x0000FF));
+  greenButton.graphics.moveTo(0,0)
+  greenButton.graphics.arc(0,0,180,Math.PI,-Math.PI/2);
+  greenButton.x=100;
+  greenButton.y=100;
+  simonGameContainer.addChild(greenButton);
+
+  var yellowButton=new createjs.Shape();
+  greenButton.graphics.f(createjs.Graphics.getRGB(0xFFFF00));
+  greenButton.graphics.moveTo(0,0)
+  greenButton.graphics.arc(0,0,180,-Math.PI/2,0);
+  greenButton.x=100;
+  greenButton.y=100;
+  simonGameContainer.addChild(greenButton);
+
+  var innerCircle=new createjs.Shape();
+  innerCircle.graphics.f(createjs.Graphics.getRGB(0xFFFFFF));
+  innerCircle.graphics.moveTo(0,0)
+  innerCircle.graphics.arc(0,0,80,0,2*Math.PI);
+  innerCircle.x=100;
+  innerCircle.y=100;
+  simonGameContainer.addChild(innerCircle);
+
+  stage.addChild(simonGameContainer);
+  simonGameContainer.x=(canvas.width-200)/2;
+  simonGameContainer.y=(canvas.height-200)/2;
+  stage.update();
+}
